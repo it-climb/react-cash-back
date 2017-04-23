1e3e495719962dd02182f987db420edfc79b5fb4
djvang <djvang92@gmail.com>
2017-04-23 14:26:31 +0300

    add titlemiddle component

---
+++ src/components/title_middle/title_middle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var TitleMiddle = React.createClass({
+
+    render: function () {
+
+        return (
+            <div className = "section-cash-back">
+                <h2>Банковский КБ</h2>
+            </div>
+        );
+    }
+});
+
+export default TitleMiddle;
