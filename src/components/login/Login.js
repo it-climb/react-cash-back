2f3ea138e5f7d80dc6eb0b3034105a15c724fd09
Olha-Vinnikova <vinnikova@online.ua>
2017-05-07 15:53:17 +0300

    add login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import {connect}  from 'react-redux';
-import { Link, browserHistory }   from 'react-router';
-import {startLogoutAction} from './../../actions/actions';
-import _ from 'lodash';
-import {Row, Col, Form, FormGroup, FormControl, Button, Alert, ControlLabel, Navbar, Table} from "react-bootstrap";
-
-
-var Login = React.createClass({
-
+'use strict';
+import React from "react";
+import {Row, Col, Form, FormGroup, FormControl, Button, Alert, ControlLabel, Navbar} from "react-bootstrap";
+import {browserHistory} from "react-router";
+import _ from "lodash";
+import SessionActions from "./../../../actions/session";
+import UserActions from "./../../../actions/user";
+export default React.createClass({
+    propTypes: {
+        currentUser: React.PropTypes.object.isRequired
+    },
+    getInitialState(){
+        return {email: '', password: '', errors: []}
+    },
     componentWillMount(){
         console.log("Login componentWilMount");
+        let {currentUser} = this.props;
+        if (!!currentUser) {
+            return browserHistory.push('/');
+        }
+    },default
+    _handleGoTo(goToPage) {
+        return browserHistory.push.bind(browserHistory, goToPage);
     },
-
-    render: function () {
-        console.log("Login.js 16");
-        return (
-            <div className = "title-top-nav">
-                Login. Привет. Ведутся рабо...
+    _submitLogin(event){
+        event.preventDefault();
+        let errors = [],
+            {email, password} = this.state,
+            $this = this;
+        if (_.trim(email).length === 0) {
+            errors.push('email');
+        }
+        if (_.trim(password).length === 0) {
+            errors.push('password');
+        }
+        if (errors.length !== 0) {
+            this.setState({errors});
+            return;
+        }
+        SessionActions.createAsync({email, password}, {})
+            .then(()=> {
+                let {redirect} = $this.props.stores.appState;
+                if (!_.isEmpty(redirect)) {
+                    window.location.replace(redirect);
+                } else {
+                    window.location.replace('/dashboard');
+                }
+            })
+            .catch(serverError=> {
+                console.log(serverError);
+                this.setState({serverError});
+            });
+    },
+    _inputChange(event){
+        let {name, value} = event.target,
+            {errors} =this.state,
+            serverError = null;
+        if (errors.indexOf(name) !== -1) {
+            let errIdx = errors.indexOf(name);
+            errors.splice(errIdx, 1);
+        }
+        this.setState({[name]: value, errors, serverError});
+    },
+    render(){
+        return <div className="test-login">
+            <header className="login-header">
+                <Navbar fluid={true}>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="/" className="brand_logo" title="logo">
+                                Cash-back
+                            </a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                </Navbar>
+            </header>
+            <div className="container">
+                <Row>
+                    <Col md={4} mdOffset={4}>
+                        <h3 className="form-header">Log Into Your  Account</h3>
+                        <Form onSubmit={this._submitLogin.bind(this)}>
+                            {
+                                this.state.serverError &&
+                                <Alert bsStyle="danger">{_.get(this.state.serverError, 'response.body.message')}</Alert>
+                            }
+                            <FormGroup
+                                controlId="email" {...(this.state.errors.indexOf('email') !== -1 ? {validationState: 'error'} : {})}>
+                                <ControlLabel>Your Email</ControlLabel>
+                                <FormControl type="text" value={this.state.email} name="email" className="text-lowercase"
+                                             onChange={this._inputChange.bind(this)}/>
+                                <FormControl.Feedback/>
+                            </FormGroup>
+                            <FormGroup
+                                controlId="password"  {...(this.state.errors.indexOf('password') !== -1 ? {validationState: 'error'} : {})}>
+                                <ControlLabel>Password</ControlLabel>
+                                <FormControl type="password" value={this.state.password} name="password"
+                                             onChange={this._inputChange.bind(this)} />
+                                <FormControl.Feedback/>
+                            </FormGroup>
+                            <input type="submit" value="Login" className="login-submit-button btn btn-main btn-big"/>
+                            <a href="#" onClick={this._handleGoTo('/forgot-password')}
+                               className="btn btn-link btn-forgot">
+                                Forgot Password?
+                            </a>
+                            <p className="text-inform">Don't have an account?&nbsp;
+                                <a href="#" onClick={this._handleGoTo('/register')}
+                                   className="btn btn-link btn-start">Get Started!</a>
+                            </p>
+                        </Form>
+                    </Col>
+                </Row>
             </div>
-        );
+        </div>
     }
 });
-
-export default Login;
 
