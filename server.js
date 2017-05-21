ba3e8db063dcb1034089f7d58ca4318b12bfe156
Bessonov.Evgeniy <bessonov.evgeniy@gmail.com>
2017-05-21 15:38:44 +0300

    Task 1. print console.log

--- server.js
+++ server.js
@@ -17,3 +17,7 @@ app.use(express.static('public'));
 app.listen(PORT, function () {
     console.log('Express server is up on port ' + PORT);
 });
+
+app.get("/test", function (req,res) {
+    console.log("I am fine");
+})
