a5fe3e269e33dac6b49e4de45deaec835fb954e1
yurii <butenko.yv@gmail.com>
2017-05-24 00:52:19 +0300

    PR.findAll({})
    findAll({})

--- src/models/test/controller.js
+++ src/models/test/controller.js
@@ -5,11 +5,7 @@ module.exports = {
 
     getAll(req, res) {
         return Tests
-            .findAll({
-                include: [{
-                    model: Tests
-                }]
-            })
+            .findAll({})
             .then((tests) => res.status(200).send(tests))
             .catch((error) => res.status(400).send(error));
     }
