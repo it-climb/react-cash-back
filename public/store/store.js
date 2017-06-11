c2ffe9d3f65b374355e8f19285b569e7c7188287
yurii <butenko.yv@gmail.com>
2017-06-11 03:45:58 +0300

    validate registration
    validate registration

--- public/store/store.js
+++ public/store/store.js
@@ -1,25 +1,36 @@
-import * as redux from 'redux';
-import thunk from 'redux-thunk';
+import reducers from "./../reducers";
+import thunk from "redux-thunk";
 
-import {usersReduser, selectedUsersReduser, authReducer, avatarReducer}  from './../reducers/redusers';
+import {
+  createStore,
+  applyMiddleware
+} from "redux";
 
-var configure = (initialState = {}) => {
 
-    var reducer = redux.combineReducers({
-        users: usersReduser,
-        selectedUser: selectedUsersReduser,
-        auth: authReducer,
-        avatar: avatarReducer
-    });
+export const store = applyMiddleware(thunk)(createStore)(reducers, {});
 
-    var _store = redux.createStore(reducer, initialState, redux.compose(
-        redux.applyMiddleware(thunk),
-        window.devToolsExtension ? window.devToolsExtension() : f => f
-    ));
-
-    return _store;
-};
-
-var store = configure();
-
-export default store;
+// import * as redux from 'redux';
+// import thunk from 'redux-thunk';
+//
+// import {usersReduser, selectedUsersReduser, authReducer, avatarReducer}  from './../reducers/redusers';
+//
+// var configure = (initialState = {}) => {
+//
+//     var reducer = redux.combineReducers({
+//         users: usersReduser,
+//         selectedUser: selectedUsersReduser,
+//         auth: authReducer,
+//         avatar: avatarReducer
+//     });
+//
+//     var _store = redux.createStore(reducer, initialState, redux.compose(
+//         redux.applyMiddleware(thunk),
+//         window.devToolsExtension ? window.devToolsExtension() : f => f
+//     ));
+//
+//     return _store;
+// };
+//
+// var store = configure();
+//
+// export default store;
